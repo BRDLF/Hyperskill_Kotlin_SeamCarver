97f5905732c4594f1c013b15bb5ba88905ca8cc3
BRDLF <afassl34@outlook.com>
2022-09-22 13:39:40 -0500

    Completed Stage 2/6: Negative Photo

--- CHANGEBLOG.md
+++ CHANGEBLOG.md
@@ -1,5 +1,13 @@
 # Hyperskill_Kotlin_Seam Carving
 
+### Stage 2/6: Negative Photo
+
+Ok. fun. Actually doing stuff with images again. Having to handle parameters. Fun stuff.
+
+I ran into a silly issue with this. IDEA said there was no `main` found. It's because I had used `fun main(args: List<String>)` instead of `fun main(args: Array<String>)`
+
+Oops! Other than that though, Easy enough.
+
 ### Stage 1/6: Create an image
 
 This was the usual Stage 1 simple setup. "Create a program that does very little towards the intended goal of the project."
--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -5,25 +5,41 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-fun main() {
-    rectangleMaker(gatherInput())
+object Carver {
+
+    fun carve(args: Array<String>) {
+        try {
+            val inFile = File(args.findArgValue("-in"))
+            val outFile = File(args.findArgValue("-out"))
+            ImageIO.write(invert(inFile), "png", outFile)
+        }
+        catch (e: RuntimeException) {
+            println(e.message)
+        }
+
+    }
+
+    private fun Array<String>.findArgValue(arg: String): String {
+        val argIndex = this.indexOf(arg)
+        if (argIndex == -1 ||
+            argIndex == this.lastIndex ||
+            this[argIndex+1].startsWith("-")) throw RuntimeException("$arg not defined")
+        else return this[argIndex + 1]
+    }
+
+    private fun invert(src: File): BufferedImage {
+        val invertedImage = ImageIO.read(src) ?: throw RuntimeException("Couldn't read $src")
+        for (x in 0 until invertedImage.width) {
+            for (y in 0 until invertedImage.height) {
+                val originalColor = Color(invertedImage.getRGB(x, y))
+                val invertedColor = Color(255 - originalColor.red, 255 - originalColor.green, 255 - originalColor.blue)
+                invertedImage.setRGB(x, y, invertedColor.rgb)
+            }
+        }
+        return invertedImage
+    }
 }
 
-fun rectangleMaker(info: Triple<Int, Int, String>){
-    val outputImage = BufferedImage(info.first, info.second, BufferedImage.TYPE_INT_RGB)
-    val graphics = outputImage.createGraphics()
-    graphics.color = Color.RED
-    graphics.drawLine(0, 0, outputImage.width - 1, outputImage.height - 1)
-    graphics.drawLine(0,outputImage.height - 1, outputImage.width - 1, 0)
-    ImageIO.write(outputImage, "png", File(info.third))
-}
-
-fun gatherInput(): Triple<Int, Int, String> {
-    println("Enter rectangle width:")
-    val width = readln().toIntOrNull()?: 1
-    println("Enter rectangle height:")
-    val height = readln().toIntOrNull()?: 1
-    println("Enter output image name:")
-    val name = readln()
-    return Triple(width,height,name)
+fun main(args: Array<String>) {
+    Carver.carve(args)
 }
