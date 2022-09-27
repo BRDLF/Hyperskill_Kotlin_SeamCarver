fbe171beacb1cd66f91de86ed24152145496d684
BRDLF <afassl34@outlook.com>
2022-09-27 16:43:31 -0500

    Completed Stage 3/6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Hyperskill_Kotlin_Seam Carving
 
+### Stage 3/6: Look at energy
+
+Program no longer returns image negative. Now returns an energy map of the image based on dual-gradient energy function.
+The energy of the pixel `(x,y)` is `E(x,y)=sqrt(Δ2x(x,y)+Δ2y(x,y))`
+
 ### Stage 2/6: Negative Photo
 
 Program instead takes 2 arguments, `-in` and `-out`
--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -4,6 +4,9 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.math.sqrt
+
+class CarverException(message: String?) : RuntimeException(message)
 
 object Carver {
 
@@ -11,9 +14,9 @@ object Carver {
         try {
             val inFile = File(args.findArgValue("-in"))
             val outFile = File(args.findArgValue("-out"))
-            ImageIO.write(invert(inFile), "png", outFile)
+            ImageIO.write(ImageIO.read(inFile).intensityMap(), "png", outFile)
         }
-        catch (e: RuntimeException) {
+        catch (e: CarverException) {
             println(e.message)
         }
 
@@ -23,12 +26,12 @@ object Carver {
         val argIndex = this.indexOf(arg)
         if (argIndex == -1 ||
             argIndex == this.lastIndex ||
-            this[argIndex+1].startsWith("-")) throw RuntimeException("$arg not defined")
+            this[argIndex+1].startsWith("-")) throw CarverException("$arg not defined")
         else return this[argIndex + 1]
     }
 
     private fun invert(src: File): BufferedImage {
-        val invertedImage = ImageIO.read(src) ?: throw RuntimeException("Couldn't read $src")
+        val invertedImage = ImageIO.read(src) ?: throw CarverException("Couldn't read $src")
         for (x in 0 until invertedImage.width) {
             for (y in 0 until invertedImage.height) {
                 val originalColor = Color(invertedImage.getRGB(x, y))
@@ -37,6 +40,61 @@ object Carver {
             }
         }
         return invertedImage
+    }
+
+    private fun BufferedImage.intensityMap(): BufferedImage {
+        var maxEnergyValue = Double.MIN_VALUE
+        val energyGrid = Array(this.width) { Array (this.height) {0.0} }
+
+        val intenseImage = BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB)
+
+        for (x in 0 until this.width) {
+            for (y in 0 until this.height) {
+                energyGrid[x][y] = this.energyOf(x, y)
+                maxEnergyValue = if (energyGrid[x][y] > maxEnergyValue) energyGrid[x][y] else maxEnergyValue
+            }
+        }
+
+        for (x in 0 until this.width) {
+            for (y in 0 until this.height) {
+                val normalizedIntensity = (255.0 * energyGrid[x][y] / maxEnergyValue).toInt()
+                intenseImage.setRGB(x, y, Color(normalizedIntensity, normalizedIntensity, normalizedIntensity).rgb)
+            }
+        }
+
+        return intenseImage
+    }
+
+    private fun BufferedImage.energyOf(passedX: Int, passedY: Int): Double {
+        //Check out of bounds
+        if (passedX < 0 || passedX >= this.width) throw CarverException("Given x of $passedX is out of bounds")
+        if (passedY < 0 || passedY >= this.height) throw CarverException("Given y of $passedY is out of bounds")
+        //Adjust for border values
+        val adjustX = when (passedX) {
+            0 -> 1
+            this.width - 1 -> passedX - 1
+            else -> passedX
+        }
+        val adjustY = when (passedY) {
+            0 -> 1
+            this.height - 1 -> passedY - 1
+            else -> passedY
+        }
+
+        val leftPixel = Color(this.getRGB(adjustX - 1, passedY ))
+        val rightPixel = Color(this.getRGB(adjustX + 1, passedY ))
+        val topPixel = Color(this.getRGB(passedX, adjustY - 1 ))
+        val bottomPixel = Color(this.getRGB(passedX, adjustY + 1 ))
+        val xRed = (leftPixel.red - rightPixel.red)
+        val xGreen = (leftPixel.green - rightPixel.green)
+        val xBlue = (leftPixel.blue - rightPixel.blue)
+        val yRed = (topPixel.red - bottomPixel.red)
+        val yGreen = (topPixel.green - bottomPixel.green)
+        val yBlue = (topPixel.blue - bottomPixel.blue)
+        val xGrad = (xRed * xRed + xGreen * xGreen + xBlue * xBlue)
+        val yGrad = (yRed * yRed + yGreen * yGreen + yBlue * yBlue)
+
+        return sqrt((xGrad + yGrad).toDouble())
     }
 }
 
